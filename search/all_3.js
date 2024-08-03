1d281e0bece8a869e061728ed257abec7f13ab36
timeester3648 <timeester3648@users.noreply.github.com>
2024-08-03 13:02:45 +0000

    deploy: 20d90cd0b25a53f3d2bf443b2c900b34ab65ccbe

--- search/all_3.js
+++ search/all_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['implementation_53',['implementation',['../classsimdutf_1_1implementation.html',1,'simdutf']]]
+  ['description_54',['description',['../classsimdutf_1_1implementation.html#af48e75e8d6d25c3c3d66334a8849cbc7',1,'simdutf::implementation']]],
+  ['detect_5fencodings_55',['detect_encodings',['../classsimdutf_1_1implementation.html#ad4a7dbe2fbc9d03da2542c07a88cdddb',1,'simdutf::implementation']]]
 ];
